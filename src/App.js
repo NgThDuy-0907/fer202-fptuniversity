45b322c2afc189a1cbe670f062260d493336a3c7
NgThDuy-0907 <duyntse182230@fpt.edu.vn>
2024-09-09 13:15:20 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React bu FPT University
+          Learn React by FPT University
         </a>
       </header>
     </div>
