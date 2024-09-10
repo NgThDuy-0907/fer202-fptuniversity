75f6b1de5a60028cda56b2885f1858613737284f
NgThDuy-0907 <duyntse182230@fpt.edu.vn>
2024-09-10 18:23:12 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React by Duy FCO
+          Learn React by EdinDzuyKo JR
         </a>
       </header>
     </div>
